25e49a315bb63752c0cfab0064806a525399f578
Sandro Groganz <sandro@campaignchain.com>
2016-07-16 12:32:20 -0700

    CampaignChain/campaignchain#192 Subsume all app-wide parameters under campaignchain_core using tree builder

--- CampaignChainChannelLinkedInBundle.php
+++ CampaignChainChannelLinkedInBundle.php
@@ -2,8 +2,13 @@
 
 namespace CampaignChain\Channel\LinkedInBundle;
 
+use CampaignChain\Channel\LinkedInBundle\DependencyInjection\CampaignChainChannelLinkedInExtension;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class CampaignChainChannelLinkedInBundle extends Bundle
 {
+    public function getContainerExtension()
+    {
+        return new CampaignChainChannelLinkedInExtension();
+    }
 }
